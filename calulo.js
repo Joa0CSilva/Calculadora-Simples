060fd8954ebf36ec5a1c53acb1da395ead0672c2
João Carvalho Silva <joaocarvalhosilvaicr@gmail.com>
2026-08-14 22:16:27 -0300

    Projeto Concluído

--- calulo.js
+++ calulo.js
@@ -1,5 +1,6 @@
 'use strict'
 
+//Soma
 const botaoSoma = document.getElementById('soma')
 
 function somar () {
@@ -12,3 +13,48 @@ function somar () {
 
 
 botaoSoma.onclick = somar
+
+
+//Subtração
+const botaoSubtracao = document.getElementById('subtracao')
+
+function subtrair () {
+    const num1 = Number(document.getElementById('numero-digitado-um').value)
+    const num2 = Number(document.getElementById('numero-digitado-dois').value)
+    const resultado = document.getElementById('resultado')
+
+    resultado.textContent = num1 - num2
+}
+
+
+botaoSubtracao.onclick = subtrair
+
+
+//Multiplicação
+const botaoMultiplicacao = document.getElementById('multiplicacao')
+
+function multiplicar () {
+    const num1 = Number(document.getElementById('numero-digitado-um').value)
+    const num2 = Number(document.getElementById('numero-digitado-dois').value)
+    const resultado = document.getElementById('resultado')
+
+    resultado.textContent = num1 * num2
+}
+
+
+botaoMultiplicacao.onclick = multiplicar
+
+
+//Divisão
+const botaoDivisao = document.getElementById('divisao')
+
+function dividir () {
+    const num1 = Number(document.getElementById('numero-digitado-um').value)
+    const num2 = Number(document.getElementById('numero-digitado-dois').value)
+    const resultado = document.getElementById('resultado')
+
+    resultado.textContent = num1 / num2
+}
+
+
+botaoDivisao.onclick = dividir
